e7563fa12d307527c2272a9937612271419058bb
CodeWizard646 <ysudhanshu646@gmail.com>
2026-06-03 16:08:39 +0530

    first commit from the the_curious_coder/branch

--- src/java8features/defaultMethods/MyChildClass.java
+++ src/java8features/defaultMethods/MyChildClass.java
@@ -6,4 +6,5 @@ public class MyChildClass implements MyInterface {
     public void print() {
         System.out.println("This is the default implementation of default method of Interface.");
     }
+
 }
